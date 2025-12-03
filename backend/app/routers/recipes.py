c7231f0a235ea707d0863bb20cad50da0f1bbed4
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-12-03 11:11:02 +0100

    utilisation enum

--- backend/app/routers/recipes.py
+++ backend/app/routers/recipes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Query
 from typing import Optional
 import json
 from shared.snowflake.client import SnowflakeClient
-
+from shared.snowflake.table.recipes_sample_table import RecipesSampleTable
 from app.models.recipe import Recipe, RecipeListResponse
 
 router = APIRouter()
@@ -53,7 +53,7 @@ async def get_recipe(recipe_id: int):
     client = SnowflakeClient()
     result = client.execute(f"""
         SELECT *
-        FROM NutriRAG_Project.DEV_SAMPLE.RECIPES_SAMPLE
+        FROM {RecipesSampleTable}
         WHERE id = {recipe_id}
      """, fetch="all")
     if len(result) > 0:
@@ -131,7 +131,7 @@ async def get_random_recipes(count: int = Query(5, ge=1, le=20)):
     # TODO: Équipe 1 - Échantillonner des recettes aléatoires
     results = client.execute(f"""
         SELECT *
-        FROM NutriRAG_Project.DEV_SAMPLE.RECIPES_SAMPLE
+        FROM {RecipesSampleTable}
         SAMPLE ({count} rows)
     """, fetch="all")
 
